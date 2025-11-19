ea8719f7e084ae4955cd3f6cf64b0ce05e8eecf2
Troy <thlong@thewildcats.net>
2025-11-18 20:46:20 -0500

    chat buttons readded

--- Game_Code/main.py
+++ Game_Code/main.py
@@ -23,7 +23,7 @@ from network import NetworkManager
 from prediction import PredictionManager
 from items import ItemManager
 import math
-import time
+import datetime
 
 pygame.init()
 # controller initialization
@@ -142,6 +142,15 @@ async def main():
 
             if event.type == pygame.KEYDOWN and game_state == "GAME":
                 global L_Can_fire, R_Can_fire, current_time, L_fire_time, R_fire_time
+                if pygame.key.get_pressed()[pygame.K_z]:
+                    current_time = datetime.datetime.now()
+                    NetworkManager.new_chat(network, "chat", {
+                        "msg": f"{input('Name: ')}- \"{input('Message: ')}\" @{current_time.strftime('%c')}\""})
+                if pygame.key.get_pressed()[pygame.K_x]:
+                    NetworkManager.get_chats(network)
+                if pygame.key.get_pressed()[pygame.K_c]:
+                    NetworkManager.delete_chat_history(network)
+
                 if event.key == pygame.K_q and L_Can_fire is True:  # Left cannon
                     L_Can_fire = False
                     L_fire_time = current_time
